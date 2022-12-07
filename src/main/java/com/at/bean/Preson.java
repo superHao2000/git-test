391b53af50f2a192abbf90238bc37b8bfb9b55fd
superHao <superHao2000@outlook.com>
2022-12-07 10:47:13 +0800

    第三次

--- src/main/java/com/at/bean/Preson.java
+++ src/main/java/com/at/bean/Preson.java
@@ -3,4 +3,28 @@ package com.at.bean;
 public class Preson {
     private Integer id;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Preson{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
