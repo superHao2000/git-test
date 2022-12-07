eaf5ee7d5c975d7d97a27014bd4e6bdf2446ff73
superHao <superHao2000@outlook.com>
2022-12-07 11:04:06 +0800

    master commit test first

--- src/main/java/com/at/bean/Preson.java
+++ src/main/java/com/at/bean/Preson.java
@@ -25,6 +25,7 @@ public class Preson {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
     }
 
     @Override
